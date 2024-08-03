2d9faf59d0c1eb37a269d85a878e53aa389372e0
jemalbrcin <23001567@myrp.edu.sg>
2024-08-04 00:50:59 +0800

    4/8/2024 12:51am

--- script.js
+++ script.js
@@ -10,3 +10,10 @@ document.getElementById('confirm').addEventListener('click', function() {
 document.getElementById('confirm').addEventListener('click', function() {
     return alert("You have successfully Top-ed Up your wallet.")
 });
+
+document.getElementById("amount").addEventListener("input", function() {
+    let amount = parseFloat(document.getElementById("amount").value);
+    let rate = 626.12;
+    let finalAmount = (amount * rate).toFixed(2);
+    document.getElementById("equivalent-amount").textContent = finalAmount;
+});
